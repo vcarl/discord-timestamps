08076299786f64e2b68dff6a0441784c40cf032a
Carl Vitullo <carl.vitullo@gmail.com>
2023-09-19 11:42:07 -0400

    Add skip buttons for 30 min increments

--- components/TimePicker.js
+++ components/TimePicker.js
@@ -168,7 +168,29 @@ export default function TimePicker({ onChange, value, className, ...props }) {
   });
 
   return (
-    <div className={className}>
+    <div className={`relative ${className}`}>
+      <div className="z-10 absolute top-0 left-0 pl-3 pt-2.5">
+        <SkipButton
+          label="-30"
+          onClick={() => {
+            const date = new Date(valueRef.current);
+            date.setMinutes(date.getMinutes() - 30);
+            onChange(date);
+          }}
+        >
+          <Left />
+        </SkipButton>
+        <SkipButton
+          label="+30"
+          onClick={() => {
+            const date = new Date(valueRef.current);
+            date.setMinutes(date.getMinutes() + 30);
+            onChange(date);
+          }}
+        >
+          <Right />
+        </SkipButton>
+      </div>
       <ReactTimePicker
         className="control"
         clearIcon={null}
@@ -188,3 +210,39 @@ export default function TimePicker({ onChange, value, className, ...props }) {
     </div>
   );
 }
+
+const SkipButton = ({ onClick, children, className = "", label, ...props }) => (
+  <button
+    {...props}
+    className={`relative opacity-90 ${className}`}
+    onClick={onClick}
+  >
+    {children}
+    <div className="absolute top-0 right-0 text-xs flex items-center justify-center h-[100%] w-[100%]">
+      {label}
+    </div>
+  </button>
+);
+
+// Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc.
+const Right = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="w-9 h-9 px-1"
+    style={{ fill: "rgba(249, 249, 249, 0.3)" }}
+    viewBox="0 0 512 512"
+  >
+    <path d="M463.5 224H472c13.3 0 24-10.7 24-24V72c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2L413.4 96.6c-87.6-86.5-228.7-86.2-315.8 1c-87.5 87.5-87.5 229.3 0 316.8s229.3 87.5 316.8 0c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0c-62.5 62.5-163.8 62.5-226.3 0s-62.5-163.8 0-226.3c62.2-62.2 162.7-62.5 225.3-1L327 183c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8H463.5z" />
+  </svg>
+);
+
+const Left = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="w-9 h-9 px-1"
+    style={{ fill: "rgba(249, 249, 249, 0.3)" }}
+    viewBox="0 0 512 512"
+  >
+    <path d="M48.5 224H40c-13.3 0-24-10.7-24-24V72c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2L98.6 96.6c87.6-86.5 228.7-86.2 315.8 1c87.5 87.5 87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3c-62.2-62.2-162.7-62.5-225.3-1L185 183c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8H48.5z" />
+  </svg>
+);
